f107c8e87fe4774830f98179d055ee41bd4b56d4
yuki-takahashi-0723 <honehonehone0723@gmail.com>
2021-01-26 20:20:34 +0900

    認証機能追加のためfirebaseのセットアップファイルを作成した

---
+++ chat/src/config/firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2D6yZFSAVsOX7FCI01blBAUYVsKbtR5E",
+    authDomain: "new-chat-app-b3a32.firebaseapp.com",
+    projectId: "new-chat-app-b3a32",
+    storageBucket: "new-chat-app-b3a32.appspot.com",
+    messagingSenderId: "1042482776604",
+    appId: "1:1042482776604:web:7f7e86ad3b598ee810edd9",
+    measurementId: "G-P0Q02B2ZBT"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  export const firebase = firebase
+  export const auth = firebase.auth()
+  export const db = firebase.firestore()
+  export const storage = firebase.storage()
+  export const timestamp = firebase.firestore.Timestamp
+
+
